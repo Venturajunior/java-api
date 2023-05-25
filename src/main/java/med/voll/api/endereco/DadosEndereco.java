4e2f0141c1c2f8edc03344fe96e5e12303570c43
Ventura Junior <you@example.com>
2023-05-25 01:09:52 -0300

    Add validation for DadosCadastroMedicos DTO

--- src/main/java/med/voll/api/endereco/DadosEndereco.java
+++ src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -1,12 +1,24 @@
 package med.voll.api.endereco;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public record DadosEndereco(
+        @NotBlank
         String logradouro,
         String numero,
         String complemento,
+        @NotBlank
         String bairro,
+        @NotBlank
         String cidade,
+        @NotBlank
+        @Size(min = 2, max = 2)
+        @Pattern(regexp = "^[A-Z]{2}")
         String uf,
+        @NotBlank
+        @Pattern(regexp = "\\d{8}")
         String cep
 ) {
 }
--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,12 +1,25 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(
+        @NotBlank
         String nome,
+        @Email
+        @NotBlank
         String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}", message = "CRM deve conter de 4 a 6 dígitos")
         String crm,
+        @NotNull
         Especialidade especialidade,
+        @NotNull
+        @Valid
         DadosEndereco endereco
 ) {
 }
